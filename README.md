11650c6f7beecddfec0adc99083d57d107f5b090
joe223 <heyin223@163.com>
2016-03-09 00:05:49 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+## A TodoList with vue.js
+
+![TodoList](http://7xrnuo.com1.z0.glb.clouddn.com/todolist.png)
+
+This is a todolist based on vue.js.
+Browser will show the message `请填写此字段` if I press the Enter button(of course, the input element is not empty),as yet, unclear.
---
+++ script.js
@@ -0,0 +1,35 @@
+Vue.config.debug = true;
+var vue  = new Vue({
+    el:'#app',
+    data:{
+        completed:true,
+        newTodo:'',
+        todolist:[
+            {
+                details:"hello world! I'm joe.",
+                completed:false,
+            },
+            {
+                details:"It was the age of wisdom,it was the age of foolishness.",
+                completed:true,
+            },
+        ],
+    },
+    methods:{
+        del:function(index){
+            this.todolist.splice(index,1);
+        },
+        add:function(){
+            if(typeof this.newTodo !== "undefined" && this.newTodo !== ''){
+                this.todolist.push({details:this.newTodo,completed:false});
+                this.newTodo = '';
+            }
+        },
+        complete:function(index) {
+            if(typeof this.todolist !== 'undefined' && typeof this.todolist[index].completed !== 'undefined'){
+                this.todolist[index].completed = !this.todolist[index].completed;
+            }
+            console.log("index:" + index + ",completed:" + this.todolist[index].completed);
+        }
+    },
+});
